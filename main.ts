ad906c7dace27e3e6f4c55880017c90718129e99
Vaughn-Hatherall <71399950+Vaughn-Hatherall@users.noreply.github.com>
2020-09-22 13:36:07 -0400

    Final commit all code is written

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.pause(500)
+        basic.showIcon(IconNames.Happy)
+        basic.pause(500)
+        basic.clearScreen()
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.pause(500)
+        basic.showIcon(IconNames.Sad)
+        basic.pause(500)
+        basic.clearScreen()
+    }
 })
